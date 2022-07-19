c4b6e6c24f6317aeef485dfc66a2f943fe77604d
Marco Donadoni <marco.donadoni@cern.ch>
2022-07-19 10:14:16 +0200

    ping: add test

---
+++ cmd/ping_test.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"reanahub/reana-client-go/utils"
+	"strings"
+	"testing"
+)
+
+func TestPing(t *testing.T) {
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if accessToken := r.URL.Query().Get("access_token"); accessToken != "1234" {
+			t.Errorf("Expected access token '1234', got '%v'", accessToken)
+		}
+		if r.URL.Path == "/api/you" {
+			w.Header().Add("Content-Type", "application/json")
+			_, err := w.Write([]byte(`{
+				"email": "john.doe@example.org",
+				"reana_server_version": "0.9.0a5"
+			}`))
+			if err != nil {
+				t.Fatalf("Error while writing response body: %v", err)
+			}
+		} else {
+			t.Fatalf("Unexpected request to '%v'", r.URL.Path)
+		}
+	}))
+	defer server.Close()
+
+	os.Setenv("REANA_SERVER_URL", server.URL)
+	defer os.Unsetenv("REANA_SERVER_URL")
+
+	rootCmd := NewRootCmd()
+	output, err := utils.ExecuteCommand(rootCmd, "ping", "-t", "1234")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !strings.Contains(output, "REANA server version: 0.9.0a5") {
+		t.Error("Expected server version in command's output")
+	}
+	if !strings.Contains(output, "Authenticated as: <john.doe@example.org>") {
+		t.Error("Expected user email in command's output")
+	}
+}
