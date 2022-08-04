76f7eb25edc7058f154c8bc5fce63a15e5a3b304
Bruno Rosendo <brunomrosendo@hotmail.com>
2022-08-04 10:59:54 +0200

    cli: move filter utils to filters.go and removed errors.go file
    Before, the utils.go was cluttered with filter functions, so that's moved to filters.go. On the other hand, errors.go only had a single function, so that was moved to utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -11,6 +11,11 @@ package utils
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
+	"net/url"
+	"os"
+	"reflect"
 	"strings"
 	"time"
 
@@ -117,4 +122,26 @@ func RemoveFromSlice[T comparable](slice []T, item T) []T {
 		}
 	}
 	return slice
+}
+
+// HandleApiError Handles API Error response which contains a payload with a message
+// Returns the original error when this doesn't happen
+func HandleApiError(err error) error {
+	_, isUrlErr := err.(*url.Error)
+	if isUrlErr {
+		return fmt.Errorf(
+			"'%s' not found, please verify the provided server URL or check your internet connection",
+			os.Getenv("REANA_SERVER_URL"),
+		)
+	}
+
+	if errValue := reflect.Indirect(reflect.ValueOf(err)); errValue.Kind() == reflect.Struct {
+		if payload := reflect.Indirect(errValue.FieldByName("Payload")); payload.Kind() == reflect.Struct {
+			if message := payload.FieldByName("Message"); message.Kind() == reflect.String {
+				return errors.New(message.String())
+			}
+		}
+	}
+
+	return err
 }
