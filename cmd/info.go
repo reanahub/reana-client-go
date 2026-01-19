390b63ca7e55616401b63cb63f3e798dc3eb64cc
Tibor Šimko <tibor.simko@cern.ch>
2026-01-19 12:33:57 +0100

    refactor(info): order output fields alphabetically (#173)
    Reorder `info` command output fields alphabetically by JSON key name to
    match the Python client's output order, ensuring consistent display
    across both clients.

--- cmd/info.go
+++ cmd/info.go
@@ -78,80 +78,15 @@ func (o *infoOptions) run(cmd *cobra.Command) error {
 			return err
 		}
 	} else {
+		// Fields are ordered alphabetically by JSON key name to match Python client
 		if p.ComputeBackends != nil {
 			displayInfoSliceItem(cmd, p.ComputeBackends.Title, p.ComputeBackends.Value)
-		}
-		if p.DefaultKubernetesJobsTimeout != nil {
-			displayInfoStringItem(cmd, p.DefaultKubernetesJobsTimeout.Title, &p.DefaultKubernetesJobsTimeout.Value)
-		}
-		if p.DefaultWorkspace != nil {
-			displayInfoStringItem(cmd, p.DefaultWorkspace.Title, &p.DefaultWorkspace.Value)
-		}
-		if p.GitlabHost != nil {
-			displayInfoStringItem(cmd, p.GitlabHost.Title, &p.GitlabHost.Value)
-		}
-		if p.DefaultKubernetesCPURequest != nil {
-			displayInfoStringItem(cmd, p.DefaultKubernetesCPURequest.Title, p.DefaultKubernetesCPURequest.Value)
-		}
-		if p.KubernetesMaxCPURequest != nil {
-			displayInfoStringItem(cmd, p.KubernetesMaxCPURequest.Title, p.KubernetesMaxCPURequest.Value)
-		}
-		if p.DefaultKubernetesCPULimit != nil {
-			displayInfoStringItem(cmd, p.DefaultKubernetesCPULimit.Title, p.DefaultKubernetesCPULimit.Value)
-		}
-		if p.KubernetesMaxCPULimit != nil {
-			displayInfoStringItem(cmd, p.KubernetesMaxCPULimit.Title, p.KubernetesMaxCPULimit.Value)
-		}
-		if p.DefaultKubernetesMemoryRequest != nil {
-			displayInfoStringItem(cmd, p.DefaultKubernetesMemoryRequest.Title, p.DefaultKubernetesMemoryRequest.Value)
-		}
-		if p.KubernetesMaxMemoryRequest != nil {
-			displayInfoStringItem(cmd, p.KubernetesMaxMemoryRequest.Title, p.KubernetesMaxMemoryRequest.Value)
-		}
-		if p.DefaultKubernetesMemoryLimit != nil {
-			displayInfoStringItem(cmd, p.DefaultKubernetesMemoryLimit.Title, p.DefaultKubernetesMemoryLimit.Value)
-		}
-		if p.KubernetesMaxMemoryLimit != nil {
-			displayInfoStringItem(cmd, p.KubernetesMaxMemoryLimit.Title, p.KubernetesMaxMemoryLimit.Value)
-		}
-		if p.MaximumInteractiveSessionInactivityPeriod != nil {
-			displayInfoStringItem(cmd, p.MaximumInteractiveSessionInactivityPeriod.Title, p.MaximumInteractiveSessionInactivityPeriod.Value)
-		}
-		if p.MaximumKubernetesJobsTimeout != nil {
-			displayInfoStringItem(cmd, p.MaximumKubernetesJobsTimeout.Title, &p.MaximumKubernetesJobsTimeout.Value)
-		}
-		if p.MaximumWorkspaceRetentionPeriod != nil {
-			displayInfoStringItem(cmd, p.MaximumWorkspaceRetentionPeriod.Title, p.MaximumWorkspaceRetentionPeriod.Value)
-		}
-		if p.WorkspacesAvailable != nil {
-			displayInfoSliceItem(cmd, p.WorkspacesAvailable.Title, p.WorkspacesAvailable.Value)
-		}
-		if p.InteractiveSessionsCustomImageAllowed != nil {
-			displayInfoStringItem(cmd, p.InteractiveSessionsCustomImageAllowed.Title, &p.InteractiveSessionsCustomImageAllowed.Value)
-		}
-		if p.InteractiveSessionRecommendedJupyterImages != nil {
-			displayInfoSliceItem(cmd, p.InteractiveSessionRecommendedJupyterImages.Title, p.InteractiveSessionRecommendedJupyterImages.Value)
-		}
-		if p.SupportedWorkflowEngines != nil {
-			displayInfoSliceItem(cmd, p.SupportedWorkflowEngines.Title, p.SupportedWorkflowEngines.Value)
 		}
 		if p.CwlEngineTool != nil {
 			displayInfoStringItem(cmd, p.CwlEngineTool.Title, &p.CwlEngineTool.Value)
 		}
 		if p.CwlEngineVersion != nil {
 			displayInfoStringItem(cmd, p.CwlEngineVersion.Title, &p.CwlEngineVersion.Value)
-		}
-		if p.YadageEngineVersion != nil {
-			displayInfoStringItem(cmd, p.YadageEngineVersion.Title, &p.YadageEngineVersion.Value)
-		}
-		if p.YadageEngineAdageVersion != nil {
-			displayInfoStringItem(cmd, p.YadageEngineAdageVersion.Title, &p.YadageEngineAdageVersion.Value)
-		}
-		if p.YadageEnginePacktivityVersion != nil {
-			displayInfoStringItem(cmd, p.YadageEnginePacktivityVersion.Title, &p.YadageEnginePacktivityVersion.Value)
-		}
-		if p.SnakemakeEngineVersion != nil {
-			displayInfoStringItem(cmd, p.SnakemakeEngineVersion.Title, &p.SnakemakeEngineVersion.Value)
 		}
 		if p.DaskEnabled != nil {
 			displayInfoStringItem(cmd, p.DaskEnabled.Title, &p.DaskEnabled.Value)
@@ -163,24 +98,90 @@ func (o *infoOptions) run(cmd *cobra.Command) error {
 			if p.DaskClusterDefaultNumberOfWorkers != nil {
 				displayInfoStringItem(cmd, p.DaskClusterDefaultNumberOfWorkers.Title, &p.DaskClusterDefaultNumberOfWorkers.Value)
 			}
-			if p.DaskClusterMaxMemoryLimit != nil {
-				displayInfoStringItem(cmd, p.DaskClusterMaxMemoryLimit.Title, &p.DaskClusterMaxMemoryLimit.Value)
-			}
 			if p.DaskClusterDefaultSingleWorkerMemory != nil {
 				displayInfoStringItem(cmd, p.DaskClusterDefaultSingleWorkerMemory.Title, &p.DaskClusterDefaultSingleWorkerMemory.Value)
-			}
-			if p.DaskClusterMaxSingleWorkerMemory != nil {
-				displayInfoStringItem(cmd, p.DaskClusterMaxSingleWorkerMemory.Title, &p.DaskClusterMaxSingleWorkerMemory.Value)
-			}
-			if p.DaskClusterMaxNumberOfWorkers != nil {
-				displayInfoStringItem(cmd, p.DaskClusterMaxNumberOfWorkers.Title, &p.DaskClusterMaxNumberOfWorkers.Value)
 			}
 			if p.DaskClusterDefaultSingleWorkerThreads != nil {
 				displayInfoStringItem(cmd, p.DaskClusterDefaultSingleWorkerThreads.Title, &p.DaskClusterDefaultSingleWorkerThreads.Value)
 			}
+			if p.DaskClusterMaxMemoryLimit != nil {
+				displayInfoStringItem(cmd, p.DaskClusterMaxMemoryLimit.Title, &p.DaskClusterMaxMemoryLimit.Value)
+			}
+			if p.DaskClusterMaxNumberOfWorkers != nil {
+				displayInfoStringItem(cmd, p.DaskClusterMaxNumberOfWorkers.Title, &p.DaskClusterMaxNumberOfWorkers.Value)
+			}
+			if p.DaskClusterMaxSingleWorkerMemory != nil {
+				displayInfoStringItem(cmd, p.DaskClusterMaxSingleWorkerMemory.Title, &p.DaskClusterMaxSingleWorkerMemory.Value)
+			}
 			if p.DaskClusterMaxSingleWorkerThreads != nil {
 				displayInfoStringItem(cmd, p.DaskClusterMaxSingleWorkerThreads.Title, &p.DaskClusterMaxSingleWorkerThreads.Value)
 			}
+		}
+		if p.DefaultKubernetesCPULimit != nil {
+			displayInfoStringItem(cmd, p.DefaultKubernetesCPULimit.Title, p.DefaultKubernetesCPULimit.Value)
+		}
+		if p.DefaultKubernetesCPURequest != nil {
+			displayInfoStringItem(cmd, p.DefaultKubernetesCPURequest.Title, p.DefaultKubernetesCPURequest.Value)
+		}
+		if p.DefaultKubernetesJobsTimeout != nil {
+			displayInfoStringItem(cmd, p.DefaultKubernetesJobsTimeout.Title, &p.DefaultKubernetesJobsTimeout.Value)
+		}
+		if p.DefaultKubernetesMemoryLimit != nil {
+			displayInfoStringItem(cmd, p.DefaultKubernetesMemoryLimit.Title, p.DefaultKubernetesMemoryLimit.Value)
+		}
+		if p.DefaultKubernetesMemoryRequest != nil {
+			displayInfoStringItem(cmd, p.DefaultKubernetesMemoryRequest.Title, p.DefaultKubernetesMemoryRequest.Value)
+		}
+		if p.DefaultWorkspace != nil {
+			displayInfoStringItem(cmd, p.DefaultWorkspace.Title, &p.DefaultWorkspace.Value)
+		}
+		if p.GitlabHost != nil {
+			displayInfoStringItem(cmd, p.GitlabHost.Title, &p.GitlabHost.Value)
+		}
+		if p.InteractiveSessionRecommendedJupyterImages != nil {
+			displayInfoSliceItem(cmd, p.InteractiveSessionRecommendedJupyterImages.Title, p.InteractiveSessionRecommendedJupyterImages.Value)
+		}
+		if p.InteractiveSessionsCustomImageAllowed != nil {
+			displayInfoStringItem(cmd, p.InteractiveSessionsCustomImageAllowed.Title, &p.InteractiveSessionsCustomImageAllowed.Value)
+		}
+		if p.KubernetesMaxCPULimit != nil {
+			displayInfoStringItem(cmd, p.KubernetesMaxCPULimit.Title, p.KubernetesMaxCPULimit.Value)
+		}
+		if p.KubernetesMaxCPURequest != nil {
+			displayInfoStringItem(cmd, p.KubernetesMaxCPURequest.Title, p.KubernetesMaxCPURequest.Value)
+		}
+		if p.KubernetesMaxMemoryLimit != nil {
+			displayInfoStringItem(cmd, p.KubernetesMaxMemoryLimit.Title, p.KubernetesMaxMemoryLimit.Value)
+		}
+		if p.KubernetesMaxMemoryRequest != nil {
+			displayInfoStringItem(cmd, p.KubernetesMaxMemoryRequest.Title, p.KubernetesMaxMemoryRequest.Value)
+		}
+		if p.MaximumInteractiveSessionInactivityPeriod != nil {
+			displayInfoStringItem(cmd, p.MaximumInteractiveSessionInactivityPeriod.Title, p.MaximumInteractiveSessionInactivityPeriod.Value)
+		}
+		if p.MaximumKubernetesJobsTimeout != nil {
+			displayInfoStringItem(cmd, p.MaximumKubernetesJobsTimeout.Title, &p.MaximumKubernetesJobsTimeout.Value)
+		}
+		if p.MaximumWorkspaceRetentionPeriod != nil {
+			displayInfoStringItem(cmd, p.MaximumWorkspaceRetentionPeriod.Title, p.MaximumWorkspaceRetentionPeriod.Value)
+		}
+		if p.SnakemakeEngineVersion != nil {
+			displayInfoStringItem(cmd, p.SnakemakeEngineVersion.Title, &p.SnakemakeEngineVersion.Value)
+		}
+		if p.SupportedWorkflowEngines != nil {
+			displayInfoSliceItem(cmd, p.SupportedWorkflowEngines.Title, p.SupportedWorkflowEngines.Value)
+		}
+		if p.WorkspacesAvailable != nil {
+			displayInfoSliceItem(cmd, p.WorkspacesAvailable.Title, p.WorkspacesAvailable.Value)
+		}
+		if p.YadageEngineAdageVersion != nil {
+			displayInfoStringItem(cmd, p.YadageEngineAdageVersion.Title, &p.YadageEngineAdageVersion.Value)
+		}
+		if p.YadageEnginePacktivityVersion != nil {
+			displayInfoStringItem(cmd, p.YadageEnginePacktivityVersion.Title, &p.YadageEnginePacktivityVersion.Value)
+		}
+		if p.YadageEngineVersion != nil {
+			displayInfoStringItem(cmd, p.YadageEngineVersion.Title, &p.YadageEngineVersion.Value)
 		}
 	}
 	return nil
